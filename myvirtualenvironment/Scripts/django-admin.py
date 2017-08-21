d66b3812cee96dad77477807692b88c4bb8e31aa
claire <recamierseven@gmail.com>
2017-08-21 10:39:20 -0400

    second commit

---
+++ myvirtualenvironment/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ibm_admin\desktop\woke-up-forum\myvirtualenvironment\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
